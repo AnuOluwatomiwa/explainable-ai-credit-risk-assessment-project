992aeedcb9fb8cbf86361618d17e887a7a528e4e
Israel <bamideleisrael04@gmail.com>
2024-05-16 23:45:00 +0100

    Update

--- App.py
+++ App.py
@@ -18,21 +18,29 @@ df_filename = "data/copy_loan_cleaned.xlsx"
 model_filename="models_and_encoders/best_loan_model(SVC).pkl"
 crs_filename="models_and_encoders/credit_risk_scaler.pkl"
 ncs_filename="models_and_encoders/new_col_scaler.pkl"
+ohe_filename="models_and_encoders/one_hot_encoder.pkl"
 
 @st.cache_data
 def get_used_df(df_filename):
     return pd.read_excel(df_filename)
 
+
+def custom_combiner(feature, category):
+    return str(feature) + "_" + str(category)
+
+
 @st.cache_resource
 def get_model(model_filename:str, 
             cr_scaler_filename:str,
-            nc_scaler_filename:str):
+            nc_scaler_filename:str,
+            ohe_filename:str):
     credit_risk_scaler = pickle.load(open(cr_scaler_filename, 'rb'))
     new_col_scaler = pickle.load(open(nc_scaler_filename, 'rb'))
+    one_hot_encoder = pickle.load(open(ohe_filename, 'rb'))
     model = pickle.load(open(model_filename, 'rb'))
-    return model, credit_risk_scaler, new_col_scaler
+    return model, credit_risk_scaler, new_col_scaler, one_hot_encoder
 
-model, credit_risk_scaler, new_col_scaler = get_model(model_filename, crs_filename, ncs_filename)
+model, credit_risk_scaler, new_col_scaler, one_hot_encoder = get_model(model_filename, crs_filename, ncs_filename, ohe_filename)
 df = get_used_df(df_filename)
 
 with center_col:
@@ -41,8 +49,8 @@ with center_col:
     dependents = st.selectbox("Number Of Dependents", options=df.Dependents.unique())
     education = st.selectbox("Education Status", options=df.Education.unique())
     self_employed = st.selectbox("Are You Self Employed", options=df.Self_Employed.unique())
-    applicant_income = st.number_input("Applicant Income", min_value=0.0)
-    coapplicant_income = st.number_input("Co-Applicant Inocome", min_value=0.0)
+    applicant_income = st.number_input("Applicant Income", min_value=1.0)
+    coapplicant_income = st.number_input("Co-Applicant Inocome", min_value=1.0)
     loan_amount = st.number_input("Loan Amount", min_value=1.0)
     loan_amt_term = st.number_input("Loan Duration (IN MONTHS)", min_value=np.array(df.Loan_Amount_Term).min(), max_value=np.array(df.Loan_Amount_Term).max())
     credit_history = st.selectbox("Credit History", options=['Yes', 'No'])
@@ -53,22 +61,26 @@ with center_col:
         infos = {
             "Gender": gender,
             "Married": married,
-            "Dependents": dependents,
+            "Dependents": str(dependents),
             "Education": education,
             "Self_Employed": self_employed,
             "ApplicantIncome": applicant_income,
             "CoapplicantIncome": coapplicant_income,
             "LoanAmount": loan_amount,
             "Loan_Amount_Term": loan_amt_term,
-            "Credit_History": credit_history,
+            "Credit_History": 1 if credit_history=='Yes' else 0,
             "Property_Area": property_area,
         }
         
         infos_df = pd.DataFrame(infos, index = [1], columns=df.columns[1:-1])
         
-        # cleaned_df = first_cleaning(infos_df, credit_risk_scaler)
-        # engineered_df = engineering(cleaned_df, new_col_scaler)
         
-        # st.table(engineered_df)
-
-        st.success("We're Almost There!!! ")
+        cleaned_df = first_cleaning(infos_df, credit_risk_scaler, one_hot_encoder)
+        
+        engineered_df = engineering(infos_df, cleaned_df, new_col_scaler)
+        
+        pred = model.predict(engineered_df)
+        
+        message = "You are Loan Eligible" if pred==1 else "You are Not Loan Eligible" if pred==0 else "Something Went Wrong!!"
+        
+        st.success(f"{message}")
--- engineering.py
+++ engineering.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
+import streamlit as st
 
-def engineering(df, new_col_scaler):
-    cleaned_df = df.copy()
-    total_income = cleaned_df['ApplicantIncome'] + cleaned_df['CoapplicantIncome']
-    loan_amt_to_total_income_ratio = cleaned_df['LoanAmount'] / total_income
+def engineering(infos_df, cleaned_df, new_col_scaler):
+    df = infos_df.copy()
+    df2 = cleaned_df.copy()
     
-    dependents = cleaned_df['Dependents'].replace('3+', 3).astype(float)
+    
+    total_income = df['ApplicantIncome'] + df['CoapplicantIncome']
+    loan_amt_to_total_income_ratio = df['LoanAmount'] / total_income
+    
+    dependents = df['Dependents'].replace('3+', 3).astype(float)
 
     total_family_members = dependents + 1
 
@@ -23,6 +27,6 @@ def engineering(df, new_col_scaler):
     new_columns_scaled = new_col_scaler.transform(new_columns_df)
     new_columns_scaled_df = pd.DataFrame(data = new_columns_scaled, index = new_columns_df.index, columns = new_columns_df.columns)
     
-    full_df_engineered = pd.concat([new_columns_scaled_df, cleaned_df], axis = 1)
+    full_df_engineered = pd.concat([new_columns_scaled_df, df2], axis = 1)
     
     return full_df_engineered
